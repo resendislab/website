cabdca776ea9b7fe89f232b39688c2c89b903732
Christian Diener <ch.diener@gmail.com>
2016-12-13 12:11:08 -0600

    deploy fixes

--- deploy.sh
+++ deploy.sh
@@ -8,5 +8,5 @@ cd public
 git init
 git add .
 git commit -m "Changes from $(date)"
-git remote add origin "https://${GH_TOKEN}@github.com/resendislab/resendislab.github.io" > /dev/null 2>&
+git remote add origin "https://${GH_TOKEN}@github.com/resendislab/${GITHUB_REPO}"
 git push -f -u origin master
