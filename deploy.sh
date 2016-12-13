845586f0989071b05d5304c4f9deabc8d2a53a5a
Christian Diener <ch.diener@gmail.com>
2016-12-13 11:21:01 -0600

    Added travis setup

---
+++ deploy.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+set -o errexit
+
+git config --global user.name "Travis CI Bot"
+git config --global user.email "support@travis-ci.com"
+
+cd public
+git init
+git add .
+git commit -m "Changes from $(date)"
+git push --force --quiet "https://${GH_TOKEN}@$github.com/${GITHUB_REPO}" > /dev/null 2>&1
